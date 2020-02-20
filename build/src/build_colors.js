7fbf1e5cc98a24e9b6ce17c3c71f675df833a37d
ac1win <grepoac34devahead@yahoo.co.jp>
2020-02-20 17:40:18 +0900

    adding path

--- build/src/build_colors.js
+++ build/src/build_colors.js
@@ -9,6 +9,7 @@ colors = Object.assign(colors,d.JSON.read("themes/src/colors/interactive.json"))
 colors = Object.assign(colors,d.JSON.read("themes/src/colors/other.json"));
 colors = Object.assign(colors,d.JSON.read("themes/src/colors/panels_tabs.json"));
 colors = Object.assign(colors,d.JSON.read("themes/src/colors/settings.json"));
+colors = Object.assign(colors,d.JSON.read("themes/src/colors/symbolicon.json"));
 colors = Object.assign(colors,d.JSON.read("themes/src/colors/terminal.json"));
 //write to 2_colors.json
 d.JSON.write("themes/src/2_colors.json",{"colors":colors})
